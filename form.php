f585ddf6b2fec21f4b89a7f1ba86001f250ff594
Nayan Gupta <56529528+nayangupta1411@users.noreply.github.com>
2021-08-26 17:48:07 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	 <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="language" content="english">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/formm.css">
+	<title>form</title>
+	 
+</head>
+<body style="background-color: #FAFAFA;">
+<div class="container-fluid col-lg-5 pt-5">
+ <div class="container-fluid center">
+      <h4 class="container-fluid pt-5 pl-4">Talk to us</h4>
+      <form method="post" action="https://getform.io/f/dea28fae-11d6-46cf-ab3e-2f121a067d95" enctype="multipart/form-data" autocomplete="off">
+        <div class="txt_field">
+          <input type="text"  name="email" required>
+          <span></span>
+          <label>Email id</label>
+        </div>
+        <div class="sec">
+        <div class="txt_field">
+          <input type="text" name="first_name" required>
+          <span></span>
+          <label>First Name</label>
+        </div>
+          <div class="txt_field ml-5">
+          <input type="text" name="last_name" required>
+          <span></span>
+          <label>last Name</label>
+        </div>
+        </div>
+        <div class="pass pb-3"> <input type="checkbox" style="background-color:#FF3147" name="consent"> I am agree to Fyle's terms and conditions and provide consent to send me communicatiion</div>
+        <input type="submit" value="Contant Us" class="mb-5" src="index.php">
+        
+      </form>
+    </div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>fyle</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="language" content="english">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/lightslider.css"/>
+    <script type="text/javascript" src="js/JQuery3.3.1.js"></script>
+    <script type="text/javascript" src="js/lightslider.js"></script>
+    
+  </head>
+  <body  >
+    <!-- 1st page -->
+    <div class="container-fluid  col-lg-8  pt-5">
+      <div class=" row">
+        <div class="col-lg-6  pt-5">
+          <div class="award pt-5">AWARD WINNING</div>
+          <div class="pt-4">
+            <h2 style="font-weight: 700;">DIGITAL MARKETING AGENCY</h2>
+          </div>
+          <div class="first_content pt-4">
+            Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque vitae ante at elit fringilla ac at purus, Morbi sed lacus nec risus finibus feugiat et fermentum
+          </div>
+          <div class="pt-4 pb-5">
+            <button class="btn btn-primary" onclick="window.location.href='form.php'">CONTACT US</button>
+          </div>
+          
+        </div>
+        <div class=" col-lg-6 pb-5 ">
+          <img src="photo/1.png" height="500vh" >
+        </div>
+      </div>
+    </div>
+    <!-- 2nd page -->
+    <!--  -->
+    <div class="container-fluid col-lg-12 pt-3" style="background-color: #FAFAFA;">
+      <div class="sec_award pt-5" style="text-align: center;">WHAT WE DO</div>
+      
+      <div class="row">
+        <div class="col-lg-2"></div>
+        <div class="col-lg-3 pt-4 pb-3">
+          <h3 style="font-weight: 600; font-size: 190%;" >SERVICE PROVIDE FOR YOU</h3>
+        </div>
+        <div class="col-lg-1 pt-4"></div>
+        <div class="col-lg-4 pt-4 first_content pb-3">
+          <div class="pt-3">
+            Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque vitae ante at elit fringilla ac at purus, Morbi sed lacus nec risus finibus feugiat et fermentum
+          </div>
+        </div>
+        
+      </div>
+    </div>
+    <!--  -->
+    <div class="pb-4">
+    <div class="container-fluid slider pb-5" style="background-color: #FAFAFA;">
+      <div class="container_">
+        <ul id="autoWidth" class="cs-hidden">
+          <li class="item-a">
+            <div class="box">
+              <img src="photo/2.png" class="model">
+              
+              <div class="details">
+                <div class="content">
+                  <div class="container-fluid boxx px-5">
+                    <img src="photo/hover.png" class="px-5 pt-5">
+                    <h6 class="pt-3">WEB DEVELOPEMENT</h6>
+                    <p class="first_content py-3">Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque</p>
+                    <button class="btn btn-primary mt-2 mb-4" onclick="window.location.href='https://www.fylehq.com/'">read more</button>
+                    <p>flye</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </li>
+          <li class="item-a">
+            <div class="box">
+              <img src="photo/3.png" class="model">
+              <div class="details">
+                <div class="content">
+                  <div class="container-fluid boxx px-5">
+                    <img src="photo/hover.png" class="px-5 pt-5">
+                    <h6 class="pt-3">WEB DEVELOPEMENT</h6>
+                    <p class="first_content py-3">Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque</p>
+                    <button class="btn btn-primary mt-2 mb-4" onclick="window.location.href='https://www.fylehq.com/'">read more</button>
+                    <p>flye</p>
+                  </div>
+                </div>
+                
+              </div>
+              
+            </div>
+          </li>
+          
+          <li class="item-a">
+            <div class="box">
+              <img src="photo/2.png" class="model">
+              <!--details-->
+              <div class="details">
+                <div class="content">
+                  <div class="container-fluid boxx px-5">
+                    <img src="photo/hover.png" class="px-5 pt-5">
+                    <h6 class="pt-3">WEB DEVELOPEMENT</h6>
+                    <p class="first_content py-3">Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque</p>
+                    <button class="btn btn-primary mt-2 mb-4" onclick="window.location.href='https://www.fylehq.com/'">read more</button>
+                    <p>flye</p>
+                  </div>
+                </div>
+                
+              </div>
+              
+            </div>
+          </li>
+          <!--4------------------------------>
+          <li class="item-a">
+            <!--slider-box-->
+            <div class="box">
+              
+              <!--model-->
+              <img src="photo/3.png" class="model">
+              <!--details-->
+              <div class="details">
+                <div class="content">
+                  <div class="container-fluid boxx px-5">
+                    <img src="photo/hover.png" class="px-5 pt-5">
+                    <h6 class="pt-3">WEB DEVELOPEMENT</h6>
+                    <p class="first_content py-3">Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque</p>
+                    <button class="btn btn-primary mt-2 mb-4" onclick="window.location.href='https://www.fylehq.com/'">read more</button>
+                    <p>flye</p>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </li>
+          
+          <li class="item-a">
+            <div class="box">
+              <img src="photo/3.png" class="model">
+              <div class="details">
+                <div class="content">
+                  <div class="container-fluid boxx px-5">
+                    <img src="photo/hover.png" class="px-5 pt-5">
+                    <h6 class="pt-3">WEB DEVELOPEMENT</h6>
+                    <p class="first_content py-3">Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque</p>
+                    <button class="btn btn-primary mt-2 mb-4" onclick="window.location.href='https://www.fylehq.com/'">read more</button>
+                    <p>flye</p>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </li>
+          <li class="item-a ">
+            <div class="box">
+              <img src="photo/4.png" class="model">
+              <div class="details">
+                <div class="content">
+                  <div class="container-fluid boxx px-5">
+                    <img src="photo/hover.png" class="px-5 pt-5">
+                    <h6 class="pt-3">WEB DEVELOPEMENT</h6>
+                    <p class="first_content py-3">Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque</p>
+                    <button class="btn btn-primary mt-2 mb-4" onclick="window.location.href='https://www.fylehq.com/'">read more</button>
+                    <p>flye</p>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </li>
+        </ul>
+        
+      </div>
+      
+    </div>
+
+ </div>
+    <!-- 3rd page -->
+<div class="container-fluid pt-5 ">
+  <div class="pt-5">
+  <div class="sec_award third pt-5">
+  <h6 class="pt-5 third">WHY CHOOSE US</h6></div>
+  </div>
+  <h2 style="font-weight: 600; font-size: 180%;" class="pt-3 third">WHY WE ARE BEST</h2>
+  <div class="container-fluid pt-5">
+    <div class="row">
+      <div class="col-lg-2"></div>
+      <div class="col-lg-2 ">
+        <img src="photo/target.png">
+        <h5 style="font-weight: 600; font-size: 105%; " class="pt-3">Clarified Vision & Target</h5>
+        <p class="pt-3">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
+      </div>
+      <div class="col-lg-2">
+        <img src="photo/high.png">
+        <div>
+          <h5 style="font-weight: 600; font-size: 105%; " class="pt-4">High Performance</h5>
+          <p class="pt-3">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
+        </div>
+      </div>
+      <div class="col-lg-2">
+        <img src="photo/maintain.png">
+        <h5 style="font-weight: 600; font-size: 105%; " class="pt-3">Maintain Security</h5>
+        <p class="pt-3">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
+      </div>
+      <div class="col-lg-2">
+        <img src="photo/better.png">
+        <h5 style="font-weight: 600; font-size: 105%; " class="pt-3">Better Strategy & Quality</h5>
+        <p class="">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- 4th page -->
+
+    <div class="container-fluid">
+      <div class="sec_award third pt-5">
+  <h6 class="pt-5 third">OUR PROJECT</h6></div>
+  <h2 style="font-weight: 600; font-size: 180%;" class="pt-3 third">WHY WE ARE BEST</h2>
+  <!--  -->
+<div class="container-fluid pt-4 pb-4">
+  <div class="row">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-6">
+      <img src="photo/5.png" id="my_image"style="height:27.5em; width:40em;">
+    </div>
+    <!--  -->
+    <div class="col-lg-4">
+      <div class="colu">
+        <button class="btn btn-succ1 px-5 pt-5" onclick="document.getElementById('my_image').src='photo/5.png'" active>
+          <h6  >Genderless Kei – Japan’s Hot</h6>
+          <p class="pt-3" style="font-size: 70%;">Set to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of</p>
+        </button>
+      </div>
+      <div class="colu">
+        <button class="btn btn-succ px-5 pt-5" onclick="document.getElementById('my_image').src='photo/5_1.png'">
+          <h6 >Better Strategy & Quality</h6>
+          <p class="pt-3" style="font-size: 70%;">Set to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of</p>
+        </button>
+      </div>
+      <div class="colu">
+        <button class="btn btn-succ px-5 pt-5" onclick="document.getElementById('my_image').src='photo/5_2.png'">
+          <h6 >Genderless Kei – Japan’s Hot</h6>
+          <p class="pt-3" style="font-size: 70%;">Set to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of</p>
+        </button>
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+</div>
+
+
+<!-- 5th -->
+
+<div class="container-fluid pb-5" style="background-color: #FAFAFA;">
+      <div class="sec_award third pt-5">
+  <h6 class="pt-5 third">EXPERTS GROWTHS</h6></div>
+  <h2 style="font-weight: 600; font-size: 180%;" class="pt-3 third">OUR COMPANY GROWTH</h2>
+<div class="container-fluid pt-5">
+  <div class="row">
+    <div class="col-lg-2"></div>
+    <div class="car col-lg-2 "> 
+      <img src="photo/heart.png" style="height:25px ; width:25px;" >
+      <h1 class="pt-3">199 +</h1>
+      <p>Staticfied Customers</p>
+    </div>
+   
+    <div class="car col-lg-2">
+      <img src="photo/time.png" style="height:25px ; width:25px;" >
+      <h1 class="pt-3">1591+</h1>
+      <p>Days Of Operation</p>
+    </div>
+   
+    <div class=" car col-lg-2">
+      <img src="photo/check.png" style="height:25px ; width:25px;" >
+      <h1 class="pt-3">283 +</h1>
+      <p>Complete Project</p>
+    </div>
+    
+    <div class="car col-lg-2">
+      <img src="photo/win.png" style="height:25px ; width:25px;" >
+      <h1 class="pt-3">75 +</h1>
+      <p>Win Awards</p>
+    </div>
+
+  </div>
+
+</div>
+
+</div>
+
+
+<!-- 6th -->
+
+<div class="container-fluid pb-5" >
+      <div class="sec_award third pt-5">
+  <h6 class="pt-5 third">CLIENTS'S FEEDBACK</h6></div>
+  <h2 style="font-weight: 600; font-size: 180%;" class="pt-3 third">PEOPLE SAY'S ABOUT US !</h2>
+  <div class="container-fluid  col-lg-6 pt-4" >
+  <img src="photo/comma.png" style="height:2em;width: 3em;">
+  <p style="margin-top:-20px">Jannat Tumpa The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
+  <p><b style="color:#FF3147">JANNAT TUMPA</b> - COO, AMERIMAR ENTERPRISES, INC.</p>
+  </div>
+  </div>
+
+
+  <!-- footer -->
+  <div class="container-fluid" style="background-image: url('photo/footer.png');">
+    <div class="container-fluid col-lg-9 p-5">
+    <img src="photo/group.png" style="width: 100%;">
+    </div>
+  </div>
+
+    <script src="js/script.js" type="text/javascript"></script>
+    <!--  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </body>
+</html>
